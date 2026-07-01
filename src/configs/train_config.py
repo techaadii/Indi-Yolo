14de17620fbabc6e77629b886e33a6e857614766
techaadii <154780246+techaadii@users.noreply.github.com>
2026-07-01 13:24:08 +0000

    feat[config]: Written the configurations file

--- src/configs/train_config.py
+++ src/configs/train_config.py
@@ -1,12 +1,31 @@
 from pathlib import Path
 
-
-
 class TrainConfig:
     """Class defining the train configurations"""
 
     # path related params
     SOURCE_DIR : str = "Add the source folder"
+    """Path to the source directory"""
+    DATASET_ROOT : str = "./vehicle_dataset"
+    """Path to the custom vehicle dataset"""
+
+    # model related params
+    MODEL : str = "Add the yolo model of your choice"
+    """Path belonging to the YOLO-MODEL variant"""
+    IMG_SIZE : tuple = (224,224)
+    """Size of the image"""
+
+    #HyperParameters
+    EPOCHS : int = 50
+    """Number of epochs in the training loop"""
+    BATCH_SIZE : int = 32
+    SPLIT_RATIO : float = 0.8
+    """Train-Test split ratio"""
+    LR : float = 0.001
+    """learning rate for the training epoch"""
+
+
+
 
 
 
