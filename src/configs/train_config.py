8e4c5ca38130d7f57f800d3f2295ed6fda50ed38
techaadii <154780246+techaadii@users.noreply.github.com>
2026-07-01 13:13:20 +0000

    dev[feat]: Added the related files and written the config

---
+++ src/configs/train_config.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+
+
+class TrainConfig:
+    """Class defining the train configurations"""
+
+    # path related params
+    SOURCE_DIR : str = "Add the source folder"
+
+
+
---
+++ src/data/datapreparation.py
@@ -0,0 +1,17 @@
+import os 
+import random 
+import shutil 
+from pathlib import Path
+
+
+def prepare_yolo_classification_data(source_path : Path, destination_path : Path) -> None:
+
+
+
+
+if __name__=="__main__":
+    SOURCE_PATH : Path= Path("Add your path")
+
+    DESTINATION_PATH : Path = Path("Add your path")
+
+    prepare_yolo_classification_data(SOURCE_PATH,DESTINATION_PATH)
