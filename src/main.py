26c68a769ff89cfd59f2edf10a76886c62696dd7
techaadii <154780246+techaadii@users.noreply.github.com>
2026-07-02 15:05:41 +0000

    dev[feat]: Added the yolo trainer

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from src.configs.train_config import TrainConfig
+from src.data.data_manager import VehicleDataManager
+from src.yolo.yolo_trainer import VehicleDatasetTrainer
+
+
+
+
+def main()->None:
+    """Main function to initiate the training loop"""
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/yolo/yolo_trainer.py
@@ -0,0 +1,33 @@
+from ultralytics import YOLO
+import wandb
+from pathlib import Path
+
+class VehicleDataTrainer:
+    """This class defines the training setup for Indi-YOLo"""
+    def __init__(self,config) -> None:
+        self.config=config
+        self.model : YOLO = YOLO(self.cfg.MODEL)
+
+
+    def train(self):
+        wandb.init(
+            project="self.cfg.PROJECT_NAME",
+            config={
+                'learning_rate':self.cfg.LR,
+                'epochs': self.cfg.EPOCHS,
+                'batch size': self.cfg.BATCH_SIZE,
+            })
+
+
+        self.model.train(
+            data=str(Path(self.cfg.DATASET_ROOT).absolute()),
+            epochs=self.cfg.EPOCHS,
+            imgsz=self.cfg.IMG_SIZE,
+            batch=self.cfg.BATCH_SIZE,
+            lr0=self.cfg.LR,
+            project=self.cfg.PROJECT_NAME,
+            
+            device="cuda"  # Set to 'cpu' if no GPU available
+        )
+        wandb.finish()
+        
